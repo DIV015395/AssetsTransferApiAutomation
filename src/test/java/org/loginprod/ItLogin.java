77008d45d31f08028ea44d334531ce54be21f6b2
ManjeetSharma <sharmaboymanjeet12@gmail.com>
2023-12-16 12:33:48 +0530

    commit .allure

--- src/test/java/org/loginprod/ItLogin.java
+++ src/test/java/org/loginprod/ItLogin.java
@@ -1,4 +1,10 @@
 package org.loginprod;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import io.qameta.allure.Description;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Link;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
@@ -8,10 +14,19 @@ import utils.EndPointFixed;
 import utils.HeaderUtilFixed;
 import org.storedatainpropertiesfile.PropertiesLoader;
 import org.testng.annotations.Test;
+
 import java.util.Map;
 
-public class ItLogin {
-    @Test()
+
+public class ItLogin
+{
+    @Test
+    @Epic("API Tests")
+    @Feature("User Authentication")
+    @Story("Doctor Login")
+    @Description("Test for doctor login using RestAssured")
+    @Owner("Your Name")
+    @Link(name = "API Documentation", type = "docs", url = "https://api.example.com/docs")
     public void testDoctorLogin()
     {
         ItLoginPojo payload = ItPayLoadBuilder.buildMyPayload();
